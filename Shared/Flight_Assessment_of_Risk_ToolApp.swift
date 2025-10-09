320537171cba051889f6945d03b1d2f2ef0dc5cb
Tim Morgan <github@timothymorgan.info>
2025-10-09 13:52:44 -0700

    Add Sentry

--- Shared/Flight_Assessment_of_Risk_ToolApp.swift
+++ Shared/Flight_Assessment_of_Risk_ToolApp.swift
@@ -1,7 +1,42 @@
+import Sentry
 import SwiftUI
 
 @main
 struct Flight_Assessment_of_Risk_ToolApp: App {
+  init() {
+    // Check for UI testing mode
+    let isUITesting = ProcessInfo.processInfo.arguments.contains("UI-TESTING")
+
+    // Only initialize Sentry if not running UI tests
+    if !isUITesting {
+      SentrySDK.start { options in
+        options.dsn =
+          "https://ca34bdbb2d92e968036855adc9831fa1@o4510156629475328.ingest.us.sentry.io/4510160946200576"
+        options.debug = true  // Enabled debug when first installing is always helpful
+
+        // Adds IP for users.
+        // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+        options.sendDefaultPii = true
+
+        // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+        // We recommend adjusting this value in production.
+        options.tracesSampleRate = 1.0
+
+        // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+        options.configureProfiling = {
+          $0.sessionSampleRate = 1.0  // We recommend adjusting this value in production.
+          $0.lifecycle = .trace
+        }
+
+        // Uncomment the following lines to add more data to your events
+        // options.attachScreenshot = true // This adds a screenshot to the error events
+        // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+
+        // Enable experimental logging features
+        options.experimental.enableLogs = true
+      }
+    }
+  }
 
   #if os(macOS)
     var body: some Scene {
