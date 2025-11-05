dc564fe0473ac069c9f868b71473bcd6fcb2b7c4
Tim Morgan <github@timothymorgan.info>
2025-11-05 10:45:30 -0800

    More efficient string manipulation

--- Shared/Extensions/String.swift
+++ Shared/Extensions/String.swift
@@ -2,10 +2,6 @@ import Foundation
 
 extension String {
   func removingCharacters(in set: CharacterSet) -> String {
-    var newString = Data(capacity: lengthOfBytes(using: .utf8))
-    for character in self.unicodeScalars where !set.contains(character) {
-      newString.append(contentsOf: character.utf8)
-    }
-    return String(data: newString, encoding: .utf8)!
+    String(unicodeScalars.filter { !set.contains($0) })
   }
 }
